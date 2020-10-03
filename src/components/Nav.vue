ffcd3bd978ccce0a4f90ee9edc8fe7395ea8fae3
mridulganga <mridul.kepler@gmail.com>
2020-10-03 18:06:40 +0530

    initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,37 @@
+  <template>
+    <div class="center examplex">
+      <vs-navbar center-collapsed v-model="active">
+        <template #left>
+          <img src="@/assets/logo-wide.png" alt="" style="height:28px;">
+        </template>
+        <vs-navbar-item :active="active == 'guide'" id="guide">
+          Home
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'docs'" id="docs">
+          About
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'components'" id="components">
+          Software
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'license'" id="license">
+          3D Printing
+        </vs-navbar-item>
+        <template #right>
+          <vs-button
+        circle
+      >Contact
+      </vs-button>
+
+        </template>
+      </vs-navbar>
+    </div>
+  </template>
+
+  <script>
+export default {
+  name: "Nav",
+  data: () => ({
+    active: "guide",
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax, {
+  // options here
+})
+
 Vue.config.productionTip = false
 
 new Vue({
